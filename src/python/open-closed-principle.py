9b32c0feab7a219d995d64f1030e8fff17088a64
Tarek Haydar <tarek@beytek.co>
2022-02-01 17:20:39 +0200

    refactor the code

--- src/python/open-closed-principle.py
+++ src/python/open-closed-principle.py
@@ -1,3 +1,7 @@
+# Open for extensions, closed for modifications.
+
+# Implementing a new way to save the person metadata requires 
+# new modifications to the 'save' method inside the 'PersonStorage' class.
 class Person:
     def __init__(self, name):
         self.name = name
@@ -7,35 +11,46 @@ class Person:
 
 
 class PersonStorage:
-    def save_to_database(self, person, save_to):
+    def save(self, person, save_to):
         if save_to == 'db':
             print(f'Save the {person} to database')
-        elif save_to == 'json':
+            return
+
+        if save_to == 'json':
             print(f'Save the {person} to a JSON file')
-        else
+            return
+            
+        else:
             print(f'invalid  storage type')
 
     
 person = Person('Tarek Haydar')
 storage = PersonStorage()
-storage.save_to_database(person, 'db')
+storage.save(person, 'db')
 
-#ABC => Abstract base class
+# Resolve making new modification with Abstractions.
+# ABC => Abstract base class
+import ABC
+
 class PersonStorage(ABC):
     @abstractmethod
     def save(self, person):
         pass
     
-class PersonDB(PersonStorage):
+class PersonStorageDB(PersonStorage):
     def save(self, person):
         print(f'Save the {person} to database')
 
 
-class PersonJSON(PersonStorage):
+class PersonStorageJSON(PersonStorage):
     def save(self, person):
         print(f'Save the {person} to a JSON file')
         
-storage = PersonXML()
+class PersonStorageXML(PersonStorage):
+    def save(self, person):
+        print(f'Save the {person} to a XML file')
+
+storage = PersonStorageXML()
 storage.save(person)
 
 
