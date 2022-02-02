30ff0866a2e1870f21b341bcb11f770583e034bb
Tarek Haydar <tarek@beytek.co>
2022-02-02 17:00:11 +0200

    add unproper code

--- src/python/encapsulation.py
+++ src/python/encapsulation.py
@@ -1,6 +1,33 @@
 # Encapsulation preserves the Object state
 # Encapsulation avoids redundancy in code and facilitates the maintenance of the code
 
+# unencapsulated the logic
+
+class AccountBank:
+
+    def __init__(self, balance, status, type):
+        self.balance = balance
+        self.status = status
+        self.type = type
+
+
+my_account = AccountBank(1000, 'Active', 'GoldenCard')
+
+
+def get_available_amount_to_withdraw(my_account):
+    if my_account.type == 'SilverCard':
+        return my_account.balance - 20
+    elif my_account.type == 'GoldenCard':
+        return my_account.balance - 10
+    else:
+        return my_account.balance
+
+
+print(my_account.get_available_amount_to_withdraw())
+
+# Encapsulating the logic
+
+
 class AccountBank:
 
     def __init__(self, balance, status, type):
